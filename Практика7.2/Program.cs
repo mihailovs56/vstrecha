94b15988e22e1bc190463f3349210f97389f0209
Михайлов <@DESKTOP-84VD5MI>
2023-11-02 11:11:07 +0500

    Добавьте файлы проекта.

---
+++ Практика7.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практика7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку: ");
+            string a = Console.ReadLine();
+            Console.WriteLine("Введите букву: ");
+            char b = Console.ReadKey().KeyChar;
+            int c = a.Count(f => (f == b));
+            Console.WriteLine();
+            Console.WriteLine("Кол-во использования символа: " + c);
+
+        }
+    }
+}
